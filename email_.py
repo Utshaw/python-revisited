ae981027697bf2503ce749056f35dbd4e871571a
Utshaw <farhan.tanvir.utshaw@gmail.com>
2020-07-30 15:10:11 +0600

    Update with gitignore

--- email_.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# Protocol for sending mail: SMTP (Simple Mail Transfer Protocol)
-import smtplib
-conn = smtplib.SMTP('smtp.gmail.com', 587) # domain, port number
-conn.ehlo() # start the connection, 
-conn.starttls()  # TLS encryption
-conn.login('d')
