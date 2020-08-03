b6c773a08e7ef202277827948e411cd9c33262a9
Utshaw <farhan.tanvir.utshaw@gmail.com>
2020-08-03 19:11:39 +0600

    Update

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ https://www.datacamp.com/community/tutorials/docstrings-python
 ## Acknowledgement
 https://www.rokomari.com/book/101763/python-porichiti
 
+# Programming Paradign (Style of Programming)
+## Procedural Programming
+- Instructs a device on how to finish a task in logical steps
+- Linear top down approach 
+- Treats data and procedures (functions/ routines) in two different entities
--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-def printHangman(numStages):
+import random
+
+def hangman(word):
+    wrong = 0
     stages = ["",
              "________        ",
              "|               ",
@@ -8,16 +11,34 @@ def printHangman(numStages):
              "|       / \     ",
              "|               "
               ]
-    for stage in stages[:numStages]:
-        print(stage)
-
-def hangman(word):
-    wrong = 0
-    
     rletters = list(word)
-    board = ["_"]*len(word)
+    board = ["__"] * len(word)
     win = False
-    
+    print("Welcome to Hangman")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "__" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n"
+              .join(stages[0: wrong]))
+        print("You lose! It was {}.".format(word))
 
-hangman([""])
 
+listOfWords = ["cat", "dog", "horse", "giraffe", "tortoise", "ant"]
+
+hangman(listOfWords[random.randint(0, len(listOfWords) - 1)])
